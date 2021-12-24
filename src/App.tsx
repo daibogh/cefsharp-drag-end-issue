8bf1332a9ecb1f94483f2ceafc075f3e2fd7cdbb
daibogh <daibogh@gmail.com>
2021-12-24 20:25:11 +0300

    feat: added defect example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useLayoutEffect, useRef} from 'react';
+import {debounce} from 'lodash'
 import './App.css';
 
 function App() {
+  const draggableRef = useRef<HTMLDivElement>(null as unknown as HTMLDivElement)
+  const logRef = useRef<HTMLPreElement>(null as unknown as HTMLPreElement)
+
+  const onDropCallback = useCallback((event) => {
+    logRef.current.textContent = logRef.current.textContent + '\n' + 'drop'
+
+  },[])
+  useLayoutEffect(() => {
+    draggableRef.current.addEventListener('dragend', (event) => {
+      logRef.current.textContent = logRef.current.textContent + '\n' + 'dragend'
+    })
+  },[])
+  const log = useCallback(debounce(() => {
+    logRef.current.textContent = logRef.current.textContent + '\n' + 'dragover'
+  }, 100),[])
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    log()
+  },[])
   return (
+      <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border: '1px solid red', width: 60, height: 20}} draggable={true} ref={draggableRef}>test</div>
+      <div style={{height: 200, width: 400, border: '1px solid grey', bottom: 0}} onDrop={onDropCallback} onDragOver={onDragOver}/>
     </div>
+    <pre ref={logRef}/>
+      </>
   );
 }
 
